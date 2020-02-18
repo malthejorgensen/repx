38afb3e56bf670a788cbf414f77e99e1e4d99c58
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2020-02-19 00:10:25 +0100

    :white_check_mark: Add first test
    Run with `python -m unittest test` inside the `/test`-directory

---
+++ test/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import subprocess
+import unittest
+
+
+class MainTestCase(unittest.TestCase):
+    def test_files(self):
+        for filename in os.listdir('infiles'):
+            if filename.endswith('.txt'):
+                res = subprocess.run(
+                    ['prex', '/$/a/', 'infiles/%s' % filename],
+                    capture_output=True,
+                    # text=True,
+                )
+                output = res.stdout
+                expected_output = ''
+                with open('outfiles/%s' % filename, 'rb') as f:
+                    expected_output = f.read()
+                self.assertEqual(output, expected_output)
